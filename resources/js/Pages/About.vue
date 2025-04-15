40b67c598723baf70b7df33bf705a5dacdb7a378
marcbustamante <marcbustamante570@gmail.com>
2025-04-15 16:57:39 +0800

    changes in about

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -13,9 +13,9 @@ import PageLayout from './PageLayout.vue';
     <PageLayout>
        <div class="bg-green-100 border border-green-300 p-8 rounded-lg w-[600px] mx-auto mt-12">
         <div class="text-center">
-            <p><strong>Name: </strong>Rica Mariz A. Gudito</p>
+            <p><strong>Name: </strong>Marc Leobenz Bustamante</p>
             <p><strong>Course & Year: </strong>BSIT - 3</p>
-            <p><strong>Cell No.: </strong> No.: 09651875873</p>
+            <p><strong>Cell No.: </strong> No.: 09602017594</p>
         </div>
        </div>
     </PageLayout>
